dca1afdb24fb8eaee4e2080765a283c27838d21b
nobuoka <nobuoka@vividcode.info>
2013-06-08 18:26:07 +0900

    Update documentation comment of `JsonValue`

--- src/main/java/info/vividcode/util/json/JsonValue.java
+++ src/main/java/info/vividcode/util/json/JsonValue.java
@@ -3,68 +3,90 @@ package info.vividcode.util.json;
 import java.math.BigDecimal;
 
 /**
- * JSON の各種値 (object, array, string, number, boolean, null)
- * に対応する Java のオブジェクトによって実装されるインターフェイス.
- * @author nobuoka
+ * The {@link JsonValue} interface is the primary datatype for the
+ * JavaScript Object Notation (JSON) structure. It represents a single
+ * value in the JSON structure. Type of a value is one of following values:
+ * string, number, boolean, null, object and array.
  *
+ * @author nobuoka
  */
 public interface JsonValue {
 
     /**
-     * JSON の各種値の型を表す列挙型
-     * @author nobuoka
-     *
+     * {@link ValueType} represents one of types of JSON value.
      */
     enum ValueType {
+        /** JSON object */
         OBJECT_VALUE,
+        /** JSON array */
         ARRAY_VALUE,
+        /** JSON number */
         NUMBER_VALUE,
+        /** JSON string */
         STRING_VALUE,
+        /** JSON boolean ({@literal true} or {@literal false}) */
         BOOLEAN_VALUE,
-        NULL_VALUE
+        /** JSON null ({@literal null}) */
+        NULL_VALUE,
     }
 
     JsonValue get(int index) throws JsonTypeException;
     JsonValue get(String key) throws JsonTypeException;
 
     /**
-     * このオブジェクトが表す JSON の値のタイプを返す.
+     * Return type of this JSON value.
+     *
+     * @return Type of this JSON value
      */
     ValueType valueType();
 
     /**
-     * JSON の array を表すオブジェクトの場合に,
-     * 対応する Java のオブジェクト ({@link JsonArray} オブジェクト) を返す.
-     * @return 対応する JsonArray オブジェクト
+     * Return itself as {@link JsonArray} if it is a {@link JsonArray},
+     * throw {@link JsonTypeException} exception otherwise.
+     * This method can be used instead of explicit casting.
+     *
+     * @return Itself
+     * @throws JsonTypeException Raised when this object is not a {@link JsonArray}
      */
     JsonArray asArray() throws JsonTypeException;
 
     /**
-     * JSON の object を表すオブジェクトの場合に,
-     * 対応する Java のオブジェクト ({@link JsonObject} オブジェクト) を返す.
-     * @return 対応する JsonObject オブジェクト
+     * Return itself as {@link JsonObject} if it is a {@link JsonObject},
+     * throw {@link JsonTypeException} exception otherwise.
+     * This method can be used instead of explicit casting.
+     *
+     * @return Itself
+     * @throws JsonTypeException Raised when this object is not a {@link JsonObject}
      */
     JsonObject asObject() throws JsonTypeException;
 
     /**
-     * JSON の number を表すオブジェクトの場合に,
-     * 対応する Java のオブジェクト ({@link BigDecimal} オブジェクト) を返す.
-     * @return 対応する BigDecimal オブジェクト
+     * Return {@link BigDecimal} representing associated number value
+     * if type of this value is number,
+     * throw {@link JsonTypeException} exception otherwise.
+     *
+     * @return {@link BigDecimal} object representing associated number value
+     * @throws JsonTypeException Raised when JSON type of this object is not number
      */
     BigDecimal numberValue() throws JsonTypeException;
 
     /**
-     * JSON の object を表すオブジェクトの場合に,
-     * 対応する Java のオブジェクト ({@link String} オブジェクト) を返す
-     * @return 対応する String オブジェクト
+     * Return {@link String} representing associated string value
+     * if type of this value is string,
+     * throw {@link JsonTypeException} exception otherwise.
+     *
+     * @return {@link String} object representing associated string value
+     * @throws JsonTypeException Raised when JSON type of this object is not string
      */
-
     String stringValue() throws JsonTypeException;
 
     /**
-     * JSON の object を表すオブジェクトの場合に,
-     * 対応する Java のオブジェクト ({@link Boolean} オブジェクト) を返す.
-     * @return 対応する Boolean オブジェクト
+     * Return {@link Boolean} representing associated boolean value
+     * if type of this value is boolean,
+     * throw {@link JsonTypeException} exception otherwise.
+     *
+     * @return {@link Boolean} object representing associated string value
+     * @throws JsonTypeException Raised when JSON type of this object is not boolean
      */
     Boolean booleanValue() throws JsonTypeException;
 
