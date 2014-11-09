e37f93969c13a40fa6e909f6cb3ee29efb6787e9
nobuoka <nobuoka@vividcode.info>
2014-11-09 22:31:31 +0900

    Refactoring of OAuthEncoder

--- src/main/java/info/vividcode/util/oauth/OAuthEncoder.java
+++ src/main/java/info/vividcode/util/oauth/OAuthEncoder.java
@@ -1,28 +1,28 @@
 package info.vividcode.util.oauth;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
- * The OAuth 1.0 Protocol の仕様に合う形で文字列をパーセントエンコードする
- * 機能を提供するクラス
- * @author nobuoka
+ * An encoder for encoding character data using percent-encoding method
+ * for OAuth 1.0 Protocol.
+ * The method is specified in
+ * <a href="http://tools.ietf.org/html/rfc5849">RFC 5849</a>.
+ *
+ * @author NOBUOKA Yu
  */
 public class OAuthEncoder {
 
-    /** "0123456789ABCDEF" の ASCII バイト列 */
-    static final private byte[] BS = { 48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70 };
-    /** 指定のバイトをパーセントエンコードする必要があるかどうかの真理値を格納した配列
-     * (インデックスがバイト値に対応. ただし最上位ビットが 1 のものは含まない) */
-    static final private boolean[] NEED_ENCODE = new boolean[0x7F];
-
-    // NEED_ENCODING の初期化
+    /** Byte codes of ASCII-encoded characters "0123456789ABCDEF". */
+    private static final byte[] BS = { 48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70 };
+    /** Elements of this array represent whether the byte specified as index need to be encoded.
+     * (Range of target bytes are just from 0x00 to 0x7F. All other bytes are need to be encoded.) */
+    private static final boolean[] NEED_ENCODE = new boolean[0x7F];
     static {
         for (int i = 0; i < NEED_ENCODE.length; i++) {
-            // a(97)-z(122), A(65)-Z(90), 0(48)-9(57), -(45), .(46), _(95), ~(126)
-            if ( ( 65 <= i && i <= 90 ) || ( 97 <= i && i <= 122) ||
-                    ( 48 <= i && i <= 57 ) || i == 45 || i == 46 || i == 95 || i == 126 ) {
+            // A(65)-Z(90), a(97)-z(122), 0(48)-9(57), -(45), .(46), _(95), ~(126)
+            if ((65 <= i && i <= 90) || (97 <= i && i <= 122) ||
+                    (48 <= i && i <= 57) || i == 45 || i == 46 || i == 95 || i == 126) {
                 NEED_ENCODE[i] = false;
             } else {
                 NEED_ENCODE[i] = true;
@@ -36,38 +36,25 @@ public class OAuthEncoder {
     private OAuthEncoder() {}
 
     /**
-     * The OAuth 1.0 Protocol の仕様に合う形で文字列をパーセントエンコードする.
-     * パーセントエンコードの対象になるのは 'A'-'Z', 'a'-'z', '0'-'9', '-', '.', '_', '~' を除く全ての文字である.
-     * @param str パーセントエンコードの対象文字列
-     * @return str をパーセントエンコードした文字列
+     * Encodes a character string using percent-encoding method
+     * for OAuth 1.0 Protocol.
+     *
+     * @param str the character string to encode
+     * @return The resulting encoded character string.
      */
-    static public String encode(String str) {
-        String encodedStr = null;
-        ByteArrayOutputStream os = null;
-        try {
-            os = new ByteArrayOutputStream();
-            for (byte b : str.getBytes( Charset.forName("UTF-8") )) {
-                if (b < 0 || NEED_ENCODE[b]) {
-                    // "%"
-                    os.write(37);
-                    // 上の 4 ビット
-                    os.write(BS[( b >> 4 ) & 0x0F]);
-                    // 下の 4 ビット
-                    os.write(BS[b & 0x0F]);
-                } else {
-                    os.write(b);
-                }
-            }
-            encodedStr = os.toString();
-        } finally {
-            try {
-                // close する意味はないが, 一応
-                if (os != null) os.close();
-            } catch (IOException err) {
-                err.printStackTrace();
+    public static String encode(String str) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream(str.length());
+        for (byte b : str.getBytes(StandardCharsets.UTF_8)) {
+            if (b < 0 || NEED_ENCODE[b]) {
+                os.write(37); // "%"
+                os.write(BS[(b >> 4) & 0x0F]); // Upper four bits
+                os.write(BS[b & 0x0F]); // Lower four bits
+            } else {
+                os.write(b);
             }
         }
-        return encodedStr;
+        return new String(os.toByteArray(), StandardCharsets.US_ASCII);
+        // ByteArrayOutputStream#close() method has no effect, so it need not be called.
     }
 
 }
