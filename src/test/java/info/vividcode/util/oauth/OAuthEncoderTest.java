7b05b61f44dbc26ca3a476567aa43279819e7df0
nobuoka <nobuoka@vividcode.info>
2014-11-09 22:23:11 +0900

    Tests for OAuthEncoder

---
+++ src/test/java/info/vividcode/util/oauth/OAuthEncoderTest.java
@@ -0,0 +1,47 @@
+package info.vividcode.util.oauth;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+
+import org.junit.Test;
+
+public class OAuthEncoderTest {
+
+    @Test
+    public void encode_unreservedCharacterSetMustNotBeEncoded() {
+        // Characters in the unreserved character set as defined by
+        // [RFC3986], Section 2.3 (ALPHA, DIGIT, "-", ".", "_", "~") MUST
+        // NOT be encoded.
+        // See: https://tools.ietf.org/html/rfc5849#section-3.6
+
+        String capitalAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        assertEquals("Capital alphabet letters are not encoded.",
+                capitalAlpha, OAuthEncoder.encode(capitalAlpha));
+
+        String lowerAlpha = capitalAlpha.toLowerCase(Locale.US);
+        assertEquals("Lower alphabet letters are not encoded.",
+                lowerAlpha, OAuthEncoder.encode(lowerAlpha));
+
+        String digits = "0123456789";
+        assertEquals("Digit letters are not encoded.",
+                digits, OAuthEncoder.encode(digits));
+
+        String unreservedSignChars = "-._~";
+        assertEquals("Unreserved sign characters are not encoded.",
+                unreservedSignChars, OAuthEncoder.encode(unreservedSignChars));
+    }
+
+    @Test
+    public void encode_otherCharactersMustBeEncoded() {
+        // *  All other characters MUST be encoded.
+        // *  The two hexadecimal characters used to represent encoded
+        //   characters MUST be uppercase.
+        // See: https://tools.ietf.org/html/rfc5849#section-3.6
+
+        String str = "(あ) good!";
+        assertEquals("All other characters are encoded.",
+                "%28%E3%81%82%29%20good%21", OAuthEncoder.encode(str));
+    }
+
+}
