d378ce1b2a71df04311c9a7c9edd98fdd2a625c4
nobuoka <nobuoka@vividcode.info>
2014-11-11 04:57:43 +0900

    Add tests

--- src/test/java/info/vividcode/util/oauth/OAuthRequestHelperTest.java
+++ src/test/java/info/vividcode/util/oauth/OAuthRequestHelperTest.java
@@ -77,6 +77,40 @@ public class OAuthRequestHelperTest {
         // URI
         String urlNotIncludingAuthQuery = helper.getUrlStringIncludeQueryParams();
         assertEquals(url, urlNotIncludingAuthQuery);
+
+        // URI including OAuth parameters
+        {
+            String urlIncludingAuthQuery = helper.getUrlStringIncludeQueryParams(true);
+            assertTrue("URL のクエリパラメータ直前まで", urlIncludingAuthQuery.startsWith(url + "?"));
+            String queryParamsStr = urlIncludingAuthQuery.substring((url + "?").length());
+            String[] params = queryParamsStr.split("&");
+            Arrays.sort(params);
+            assertArrayEquals(new String[] {
+                    "oauth_callback=oob",
+                    "oauth_consumer_key=TEST_KEY%3D%3D",
+                    "oauth_nonce=faQeA8ae22",
+                    "oauth_signature=e%2FDKfY2wLhBB9yqj5WtoJv2Ff6w%3D",
+                    "oauth_signature_method=HMAC-SHA1",
+                    "oauth_timestamp=1368908614",
+                    "oauth_version=1.0",
+            }, params);
+        }
+
+        // HTTP Request body including OAuth parameters
+        {
+            String reqBodyIncludingAuthQuery = helper.getRequestBodyString(true);
+            String[] params = reqBodyIncludingAuthQuery.split("&");
+            Arrays.sort(params);
+            assertArrayEquals(new String[] {
+                    "oauth_callback=oob",
+                    "oauth_consumer_key=TEST_KEY%3D%3D",
+                    "oauth_nonce=faQeA8ae22",
+                    "oauth_signature=e%2FDKfY2wLhBB9yqj5WtoJv2Ff6w%3D",
+                    "oauth_signature_method=HMAC-SHA1",
+                    "oauth_timestamp=1368908614",
+                    "oauth_version=1.0",
+            }, params);
+        }
     }
 
 }
